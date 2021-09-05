871c2b59e51f25da8497f46c0c5eef20380ab85e
GiangGiang1105 <nnhgiang.17it1@vku.udn.vn>
2021-09-05 15:12:06 +0700

    open class student version 3

--- aidllibrary/src/main/java/com/example/aidllibrary/entity/Student.kt
+++ aidllibrary/src/main/java/com/example/aidllibrary/entity/Student.kt
@@ -4,13 +4,4 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-open class Student(
-    var id: Int = 0,
-    var name: String = "",
-    var age: Int = 0,
-    var math: Float = 0f,
-    var physical: Float = 0f,
-    var chemistry: Float = 0f,
-    var english: Float = 0f,
-    var literature: Float = 0f
-) : Parcelable
+open class Student() : Parcelable
