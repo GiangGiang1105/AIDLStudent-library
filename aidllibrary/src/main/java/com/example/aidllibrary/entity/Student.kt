2083cf1d6e640262b35eea9c21b88bb9afad1744
GiangGiang1105 <nnhgiang.17it1@vku.udn.vn>
2021-09-06 08:13:32 +0700

    update object student

--- aidllibrary/src/main/java/com/example/aidllibrary/entity/Student.kt
+++ aidllibrary/src/main/java/com/example/aidllibrary/entity/Student.kt
@@ -4,4 +4,13 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-open class Student() : Parcelable
+data class Student(
+    var id: Int = 0,
+    var name: String = "",
+    var age: Int = 0,
+    var math: Float = 0f,
+    var physical: Float = 0f,
+    var chemistry: Float = 0f,
+    var english: Float = 0f,
+    var literature: Float = 0f
+) : Parcelable
